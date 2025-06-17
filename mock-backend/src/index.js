7ed3696760e3b96ef1735314b9172fa7cb489f2e
emailkolar <emilaalund.nyboder@gmail.com>
2025-06-17 12:34:25 +0200

    changed prisma to js

--- mock-backend/src/index.js
+++ mock-backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import prisma from "./prisma.ts"; 
+import prisma from "./prisma.js"; 
 
 const app = express();
 const PORT = process.env.PORT || 3002;
